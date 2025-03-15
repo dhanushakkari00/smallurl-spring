7ffe3a20cc8d3bf0b435d64da370d69a4e55eda9
dhanushakkari00 <130866250+dhanushakkari00@users.noreply.github.com>
2025-03-16 01:29:20 +0530

    Added MongoDB model URLMapping

--- src/main/java/com/example/smallurl/model/Url.java
+++ src/main/java/com/example/smallurl/model/Url.java
@@ -1,11 +1,60 @@
 package com.example.smallurl.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
+import java.time.Instant;
 
-@Data
-@Document(collection = "urls")
+
+@Document(collection = "short_urls")
 public class Url {
+    @Id
+    private String id;
+    private String originalUrl;
+    private String shortcode;
+    private Instant createdAt;
+    private Instant expireAt;
+
+    public Url(String originalUrl , String shortcode, Instant expireAt){
+        this.originalUrl = originalUrl;
+        this.shortcode = shortcode;
+        this.expireAt = expireAt;
+        this.createdAt = Instant.now();
+    }
+    public String getID(){
+        return id;
+    }
+    public String getOriginalUrl(){
+        return originalUrl;
+
+    }
+    public String getShortCode(){
+        return shortcode;
+    }
+    public Instant getCreatedAt(){
+        return createdAt;
+    }
+    public Instant getExpiresAt(){
+        return expireAt;
+    }
+    public void setOriginalUrl(String originalUrl){
+        this.originalUrl = originalUrl;
+    }
+    public void setExpireAt(Instant expireAt){
+        this.expireAt = expireAt;
+    }
+    public void setShortCode(String shortcode){
+        this.shortcode = shortcode;
+    }
+    @Override
+    public String toString() {
+        return "ShortUrl{" +
+                "id='" + id + '\'' +
+                ", originalUrl='" + originalUrl + '\'' +
+                ", shortCode='" + shortcode + '\'' +
+                ", createdAt=" + createdAt +
+                ", expiresAt=" + expireAt +
+                '}';
+    }
 
 }
